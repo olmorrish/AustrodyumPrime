e9f52840040166046408d591878dfa63bd2ff9cd
BuildTools <olmorris@ucalgary.ca>
2021-02-15 16:33:32 -0700

    Initial Commit
    Fonts and some basic scripts are present.

---
+++ Assets/AlwaysSelected.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysSelected : MonoBehaviour {
+
+    private TMPro.TMP_InputField inputField;
+
+    private void Start() {
+        inputField = GetComponent<TMPro.TMP_InputField>();
+    }
+
+    void Update() {
+        //ensure the field is always the focus
+        inputField.Select();
+        inputField.ActivateInputField();
+    }
+
+}
